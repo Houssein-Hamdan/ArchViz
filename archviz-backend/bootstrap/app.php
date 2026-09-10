f940a9a2959434d1ccee6a90003d4c93156b5839
Houssein-Hamdan <houssein.hamdn@gmail.com>
2026-09-10 17:22:32 +0300

    fix 13

--- archviz-backend/bootstrap/app.php
+++ archviz-backend/bootstrap/app.php
@@ -1,9 +1,10 @@
-
 <?php
 
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -13,6 +14,21 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
+
+        $middleware->prepend(function (Request $request, $next) {
+
+            if ($request->isMethod('OPTIONS')) {
+                return response('', 204)
+                    ->header('Access-Control-Allow-Origin', 'https://arch-viz-nine.vercel.app')
+                    ->header('Access-Control-Allow-Credentials', 'true')
+                    ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS')
+                    ->header('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With, Accept')
+                    ->header('Access-Control-Max-Age', '3600');
+            }
+
+            return $next($request);
+        });
+
         $middleware->append(
             \Illuminate\Http\Middleware\HandleCors::class
         );
